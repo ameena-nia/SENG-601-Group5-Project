5ae7bc524281fbf16dd5d812e3d81271d4d3436a
ameena-nia <155574888+ameena-nia@users.noreply.github.com>
2025-11-26 17:07:23 -0500

    Added sections to the home page with little bit of styling.

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -7,8 +7,25 @@ export default function Home () {
         <div className="home">
             
          <div className="homepage">
-            <h1>THE DILEEMAS WITH <br />FACIAL RECOGNITION TECHNOLOGY</h1>
-            <p>Facial Recognition Technology is an innovative means <br />of easily recognizing people via software <br />to identify and secure. Sum sum sum sum sum sum </p>
+            <h1>THE ETHICAL DILEMMA OF <br />FACIAL RECOGNITION TECHNOLOGY</h1>
+            <p id='authors'>By: Chelsea Okoroji, Garrett Mizell, Rediat Mulualem, Ameena Niamathullah</p>
+            <img src={facialtech} alt="Facial Recognition Technology" />
+
+            <section id="what-is-facial-recognition-technology">    
+                <h2>What is Facial Recognition?</h2>
+                <p>Facial Recognition Technology is an innovative means <br />
+                of easily recognizing people via software <br />
+                to identify and secure. Sum sum sum sum sum sum </p>
+            </section>
+
+            <section id="ethical-concerns">
+                <h2>Key Ethical Concerns</h2>
+            </section>
+
+            <section id="real-world-applications">
+                <h2>Real World Applications</h2>
+            </section>
+
         </div>
         </div>
     );

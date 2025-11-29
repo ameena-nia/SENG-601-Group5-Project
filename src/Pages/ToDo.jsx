d09b3b5eefa06cf380f2f2674cd495d5ba490783
ameena-nia <155574888+ameena-nia@users.noreply.github.com>
2025-11-29 13:57:21 -0500

    Working on ToDo header section.

--- src/Pages/ToDo.jsx
+++ src/Pages/ToDo.jsx
@@ -5,6 +5,43 @@ export default function ToDo (){
     return (
         <div class="todo-page">
             <h1>POTENTIAL SOLUTIONS</h1>
+            <div class="todo-header">    
+                <p>We stated previously that the main ethical concerns with facial recognition algorithms are:</p>
+                <ul>
+                    <li><a href="#privacy">Privacy Violations</a></li>
+                    <li><a href="#bias">Bias and Discrimination</a></li>
+                    <li><a href="#transparency">Lack of Transparency</a></li>
+                    <li><a href="#autonomy">User Autonomy and Consent</a></li>
+                    <li><a href="#risk">Security Risk</a></li>
+                </ul>
+                <p>To address these issues, we have compiled a list of solutions and legal and policy changes that stakeholders can take to make facial recognition technology more safe for everybody to use.</p>  
+            </div>
+
+            <section id="solutions">
+                <section id="privacy">
+                    <h3>Privacy Violation</h3>
+                </section>
+
+                <section id="bias">
+                    <h3>Bias and Discrimination</h3>
+                    <p>blah blah blah</p>
+                </section>
+
+                <section id="transparency">
+                    <h3>Lack of Transparency</h3>
+                    <p>blah blah blah</p>
+                </section>
+
+                <section id="autonomy">
+                    <h3>User Autonomy and Consent</h3>
+                    <p>blah blah blah</p>
+                </section>
+
+                <section id="risk">
+                    <h3>Security Risk</h3>
+                    <p>blah blah blah</p>
+                </section>
+            </section>
         </div>
 
     );

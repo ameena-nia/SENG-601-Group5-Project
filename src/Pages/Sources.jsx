76287587ca7faebca859c816d72ac454e9d200f0
ameena-nia <155574888+ameena-nia@users.noreply.github.com>
2025-12-01 14:49:36 -0500

    Finalized citations

--- src/Pages/Sources.jsx
+++ src/Pages/Sources.jsx
@@ -7,47 +7,41 @@ export default function Sources (){
             <h1>SOURCES</h1>
             <div id="sources-list">
                 <ul>
-                    <li>American Civil Liberties Union. (2020, June 24). Wrongfully accused by an algorithm.
-                        https://www.aclu.org/news/privacy-technology/wrongfully-accused-by-an-algorithm</li>
-                    <li>Garvie, C. (2021). A national agenda for facial recognition accountability. Georgetown Law Center on Privacy & Technology.
-                         https://www.privacyandtech.org/</li>
-                    <li>Hesson, T. (2025, October 24). U.S. expands facial recognition at borders to track non-citizens. Reuters. 
-          =              https://www.reuters.com/world/us/us-expands-facial-recognition-borders-track-non-citizens-2025-10-24/</li>
+                    <li>American Civil Liberties Union. (2020, June 24). Wrongfully accused by an algorithm. https://www.aclu.org/news/privacy-technology/wrongfully-accused-by-an-algorithm</li>
 
-                    <li>Hill, K. (2020, January 18). The secretive company that might end privacy as we know it. The New York Times. 
-                        https://www.nytimes.com/2020/01/18/technology/clearview-privacy-facial-recognition.html</li>
-
-                    <li>Leslie, D. (2020). Understanding bias in facial recognition technologies. SSRN Electronic Journal. 
-                        https://doi.org/10.2139/ssrn.3705658</li>
-
-                    <li>National Academies of Sciences, Engineering, and Medicine. (n.d.). Facial recognition technology: Project overview. 
-                        https://www.nationalacademies.org/projects/DEPS-CSTB-21-04</li>
-
-                    <li>National Institute of Standards and Technology. (2019). Face Recognition Vendor Test (FRVT): Part 3—Demographic effects 
-                        (NISTIR 8280). U.S. Department of Commerce. https://doi.org/10.6028/NIST.IR.8280</li>
-
-                    <li>New York State Bar Association. (2023). Privacy vs. security: The legal implications of using facial recognition technology at entertainment venues.
-                         https://nysba.org/privacy-vs-security-the-legal-implications-of-using-facial-recognition-technology-at-entertainment-venues/?srsltid=AfmBOoo6UNjYEUw9YIIu14NWeMRrYZ78QoKiEaArh0-RBaCn8xOtjaGf</li>
-
-                    <li>Santa Clara University. (n.d.). Examining the ethics of facial recognition. 
-                        https://www.scu.edu/ethics/focus-areas/technology-ethics/resources/examining-the-ethics-of-facial-recognition/</li>
-
-                    <li>U.S. Commission on Civil Rights. (2024). U.S. Commission on Civil Rights releases report on civil rights implications of federal use of facial recognition technology.
-                         https://www.usccr.gov/news/2024/us-commission-civil-rights-releases-report-civil-rights-implications-federal-use-facial</li>
-
-                     <li>Luxand. (2024). Police facial recognition: Safe or scary? What you need to know. Luxand Cloud. https://luxand.cloud/face-recognition-blog/police-facial-recognition-safe-or-scary-what-you-need-to-know-2024</li>
-
-                    <li>Association for Computing Machinery. (2018). ACM Code of Ethics and Professional Conduct. https://www.acm.org/code-of-ethics</li>
+                    <li>Association for Computing Machinery. (2018). ACM Code of Ethics and Professional Conduct. https://www.acm.org/code-of-ethics</li>
 
                     <li>CBS New York. (2025). Facial recognition technology error led to wrongful arrest, Brooklyn father says. CBS New York. https://www.cbsnews.com/newyork/news/nyc-facial-recognition-technology-wrongful-arrest-indecent-exposure/</li>
 
-                    <li>Cranmore, C. (2025). Black man falsely jailed after failure of NYPD’s facial recognition surveillance tech. ABC7 New York. https://abc7ny.com/17664671/</li>
+                    <li>Cranmore, C. (2025). Black man falsely jailed after failure of NYPD’s facial recognition surveillance tech. ABC7 New York. https://abc7ny.com/17664671/</li>
+
+                    <li>Garvie, C. (2021). A national agenda for facial recognition accountability. Georgetown Law Center on Privacy & Technology. https://www.privacyandtech.org/</li>
+
+                    <li>Hesson, T. (2025, October 24). U.S. expands facial recognition at borders to track non-citizens. Reuters. https://www.reuters.com/world/us/us-expands-facial-recognition-borders-track-non-citizens-2025-10-24/</li>
+
+                    <li>Hill, K. (2020, January 18). The secretive company that might end privacy as we know it. The New York Times. https://www.nytimes.com/2020/01/18/technology/clearview-privacy-facial-recognition.html</li>
+
+                    <li>Leslie, D. (2020). Understanding bias in facial recognition technologies. SSRN Electronic Journal. https://doi.org/10.2139/ssrn.3705658</li>
+
+                    <li>Luxand. (2024). Police facial recognition: Safe or scary? What you need to know. Luxand Cloud. https://luxand.cloud/face-recognition-blog/police-facial-recognition-safe-or-scary-what-you-need-to-know-2024</li>
+
+                    <li>Lyu, G., & Lyu, G. (2024, December 28). Facial Recognition Technologies | The Regulatory Review. The Regulatory Review. https://www.theregreview.org/2024/12/28/seminar-facial-recognition-technologies/#:~:text=Facial%20recognition%20technology%20is%20a%20controversial%20tool,Tracking%20work%20attendance%20*%20Entering%20amusement%20parks</li>
 
                     <li>Manis, E., Cahn, A. F., Akyol, N., & Magee, C. (2021). Scan City: A decade of NYPD facial recognition abuse (White paper). Surveillance Technology Oversight Project.</li>
+
+                    <li>National Academies of Sciences, Engineering, and Medicine. (n.d.). Facial recognition technology: Project overview. https://www.nationalacademies.org/projects/DEPS-CSTB-21-04</li>
+
+                    <li>National Institute of Standards and Technology. (2019). Face Recognition Vendor Test (FRVT): Part 3—Demographic effects (NISTIR 8280). U.S. Department of Commerce. https://doi.org/10.6028/NIST.IR.8280</li>
+
+                    <li>New York State Bar Association. (2023). Privacy vs. security: The legal implications of using facial recognition technology at entertainment venues. https://nysba.org/privacy-vs-security-the-legal-implications-of-using-facial-recognition-technology-at-entertainment-venues/?srsltid=AfmBOoo6UNjYEUw9YIIu14NWeMRrYZ78QoKiEaArh0-RBaCn8xOtjaGf</li>
+
+                    <li>Raposo, V. L. (2024). Facial recognition AI technology in healthcare and the law. In B. Solaiman & I. G. Cohen (Eds.), Research Handbook on Health, AI and the Law (Chapter 3). Edward Elgar Publishing Ltd. https://www.ncbi.nlm.nih.gov/books/NBK613200/ doi:10.4337/9781802205657.ch03</li>
+
+                    <li>Santa Clara University. (n.d.). Examining the ethics of facial recognition. https://www.scu.edu/ethics/focus-areas/technology-ethics/resources/examining-the-ethics-of-facial-recognition/</li>
+
+                    <li>U.S. Commission on Civil Rights. (2024). U.S. Commission on Civil Rights releases report on civil rights implications of federal use of facial recognition technology. https://www.usccr.gov/news/2024/us-commission-civil-rights-releases-report-civil-rights-implications-federal-use-facial</li>
                 </ul>                
             </div>
-
         </div>
-
     );
 }
